d1711428525a65410316caa907fd41beb5604cb8
David Gomes <davidrafagomes@gmail.com>
2014-05-04 01:14:43 +0100

    The API Wrapper now uses proper JSON objects.

--- app/src/main/java/com/jeknowledge/hojehapraxe/app/HojeHaPraxeAPI.java
+++ app/src/main/java/com/jeknowledge/hojehapraxe/app/HojeHaPraxeAPI.java
@@ -2,6 +2,8 @@ package com.jeknowledge.hojehapraxe.app;
 
 import android.os.AsyncTask;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -16,26 +18,42 @@ public class HojeHaPraxeAPI extends AsyncTask<Void, Void, String> {
     protected String doInBackground(Void... params) {
         URL praxeURL;
         URLConnection praxeConnection;
-        BufferedReader in;
-        String result = "Erro.";
+        BufferedReader reader;
+        String rawAPI = "Erro.";
+        JSONObject jObject = null;
 
         try {
             praxeURL = new URL("http://praxe.herokuapp.com/result");
             praxeConnection = praxeURL.openConnection();
-            in = new BufferedReader(new InputStreamReader(praxeConnection.getInputStream()));
+            reader = new BufferedReader(new InputStreamReader(praxeConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
 
-            String inputLine = in.readLine();
-            in.close();
-
-            if (inputLine.contains("false")) {
-                result = "Não há praxe.";
-            } else if (inputLine.contains("true")) {
-                result = "Há praxe.";
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
             }
+
+            reader.close();
+            rawAPI = sb.toString();
+
+            jObject = new JSONObject(rawAPI);
         } catch (Exception ex) {
             System.out.println("An error occurred: " + ex.toString());
         }
 
-        return result;
+        boolean result = false;
+
+        try {
+            result = jObject.getBoolean("hapraxe");
+        } catch (Exception ex) {
+            System.out.println("An error occurred: " + ex.toString());
+            return "Erro: " + ex.toString();
+        }
+
+        if (result) {
+            return "Há praxe";
+        }
+
+        return "Não há praxe";
     }
 }
